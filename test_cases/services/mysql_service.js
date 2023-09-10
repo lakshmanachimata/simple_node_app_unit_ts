09e1c42b9a1dadf0eeb386f2f60751388cb1b150
Your Name <you@example.com>
2023-09-10 16:29:47 +0530

    removed db logging

--- test_cases/services/mysql_service.js
+++ test_cases/services/mysql_service.js
@@ -27,7 +27,6 @@ const connectSDb = async (host , user ,password,database,dialect,storage) => {
       const sequelize = initDB(host , user ,password,database,dialect, storage)
       try {
         await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
         return sequelize
       } catch (error) {
         console.error('Unable to connect to the database:', error);
